0297b6dec51a238bb5e12ff4be1bb3a9f7f78016
Varun Garade <varungarade1151@gmail.com>
2023-05-22 17:32:03 +0530

    Dashboard in progress

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import LoginForm  from './Components/LoginForm.js';
 import About from './Components/About';
 import Navbar from './Components/Navbar';
 import ErrorPage from './Components/ErrorPage';
+import Dashboard from './Components/Dashboard';
+import Calender from './Components/Calender';
 
 function App() {
   return (
@@ -17,7 +19,8 @@ function App() {
           <Route path='/' element={<LoginForm></LoginForm>} />
           <Route path='/about' element={<About/>} />
           <Route path='/error' element={<ErrorPage/>} />
-          
+          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/cal' element={<Calender/>} />
         </Routes>
         </BrowserRouter>
     </>
