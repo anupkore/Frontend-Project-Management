caed7267cb75d0d00e5c4f20382b156a928ad3d7
Varun Garade <varungarade1151@gmail.com>
2023-05-25 16:40:06 +0530

    Issues UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,15 @@ module.exports = {
         "dark-purple": "#081A51",
         "light-white": "rgba(255,255,255,0.17)",
       },
-    },
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    }, 
   },
-  plugins: [],
+  plugins: [
+    // ...
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
 
