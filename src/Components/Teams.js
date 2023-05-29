47dc34822acd470371476e46d68be04d9a62bee6
Yogesh Baiskar <yogeshjbaiskar100@gmail.com>
2023-05-29 16:31:46 +0530

    Type Field removed

--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -280,7 +280,6 @@ export const Teams = () => {
               <th className="px-4 py-2">Name</th>
               <th className="px-4 py-2">Email</th>
               <th className="px-4 py-2">Role</th>
-              <th className="px-4 py-2">Type</th>
               <th className="px-4 py-2">Satrt Date</th>
               <th className="px-4 py-2">End Date</th>
               <th className="px-4 py-2">Issues</th>
@@ -313,7 +312,6 @@ export const Teams = () => {
               <td className="px-4 py-2">{issue.assignedTo[0].name}</td>
               <td className="px-4 py-2">{issue.assignedTo[0].email}</td>
               <td className="px-4 py-2">{issue.assignedTo[0].role}</td>
-              <td className="px-4 py-2">{issue.type}</td>
               <td className="px-4 py-2">{issue.startDate}</td>
               <td className="px-4 py-2">{issue.endDate}</td>
               <td className="px-4 py-2 underline text-blue-900">
