59ea21f15368091ce2df4f2c7a3dc554b4923a7f
Yogesh Baiskar <yogeshjbaiskar100@gmail.com>
2023-05-29 18:20:33 +0530

    All Project Page redesign and Fliters added and Change Card Format to Table Format

--- src/Components/AllProject.js
+++ src/Components/AllProject.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import FormDialog from "./Dialog";
 
@@ -67,54 +67,109 @@ const projects1 = [
 
 
 
+
 export const AllProjectList = () => {
-  const maxWidth='md';
+  const maxWidth = 'md';
+  const [filterStatus, setFilterStatus] = useState("All");
+
+  const filteredProjects = projects1.filter((project) => {
+    if (filterStatus === "All") {
+      return true;
+    } else {
+      return project.status === filterStatus;
+    }
+  });
+
+  const handleFilterChange = (event) => {
+    setFilterStatus(event.target.value);
+  };
 
   return (
-    <>
-      <section
-        className="  bg-blur-3xl bg-opacity-30 "
-        style={{
-          backgroundColor: "",
-          margin: "5%",
-          borderRadius: "10px",
-          padding: "1rem",
-          backgroundImage: `url(${bgProject})`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    width: "90vw", // Adjust the width to fit the screen horizontally
-    height: "100vh", // Adjust the height to fit the screen vertically
-        }}
-      >
-     <div className="flex py-4 px-5 justify-between items-center">
-  <h1 className="text-center mb-0 flex-grow-1 mb-2" style={{ fontFamily: "Arial", fontSize: "24px" }}>
-    <span className="bg-white px-4 py-2 rounded-md shadow-md text-navy-blue align-items-center mx-auto text-center">
-      My Projects
-    </span>
-  </h1>
-  <div className="ml-auto">
-    <FormDialog prop={<AddNewProject></AddNewProject>} style={maxWidth} buttonTitle="Create Project" />
-  </div>
-</div>
+    <section
+      className="bg-blur-3xl bg-opacity-30"
+      style={{
+        backgroundColor: "",
+        margin: "5%",
+        borderRadius: "10px",
+        padding: "1rem",
+        backgroundImage: `url(${bgProject})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        width: "90vw", // Adjust the width to fit the screen horizontally
+        // height: "100vh", // Adjust the height to fit the screen vertically
+      }}
+    >
+      <div className="py-4 px-5 flex ">
+        <h1
+          className="text-center mb-0 flex-grow-1 mb-2 items-center text-xl font-bold"
+          style={{ fontFamily: "Arial",marginLeft:"80px" }}
+        >
+          <span className="bg-white px-2 py-2 rounded-md shadow-md text-navy-blue ">
+            My Projects
+          </span>
+        </h1>
+        <div className="ml-auto justify-right">
+          <FormDialog
+            prop={<AddNewProject />}
+            style={maxWidth}
+            buttonTitle="Create Project"
+          />
+        </div>
+      </div>
 
+      <div className="flex justify-left mt-1">
+        <label htmlFor="filterStatus" className="mr-2">
+          Filter by Status:
+        </label>
+        <select
+  id="filterStatus"
+  className="px-2 py-1 border rounded-md mr-5 text-sm"
+  style={{ paddingRight: '1.1rem', width: '8rem' }}
+  value={filterStatus}
+  onChange={handleFilterChange}
+>
+  <option value="All">All Projects</option>
+  <option value="In Progress">In Progress</option>
+  <option value="Completed">Completed</option>
+  <option value="Planned">Planned</option>
+</select>
+      </div>
 
-        
-        <div className=" grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 ">
-       
-        {projects1.map((project) =>(
-            <ProjectCard
-              id={project.id}
-              title={project.title}
-              startDate={project.startDate}
-              endDate={project.endDate}
-              status={project.status}
-              description={project.description}
-            />
-          ))}
+      <section>
+        <div className="flex flex-col items-center mt-4">
+          <table className="table-auto w-full mx-auto bg-white rounded-md shadow-md">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">Sr.No</th>
+                <th className="px-4 py-2">Name</th>
+                <th className="px-4 py-2">Status</th>
+                <th className="px-4 py-2">Start Date</th>
+                <th className="px-4 py-2">End Date</th>
+                <th className="px-4 py-2">Description</th>
+                <th className="px-4 py-2">Details</th>
+                {/* <th className="px-4 py-2">Issues</th> */}
+              </tr>
+            </thead>
+            <tbody>
+              {filteredProjects.map((project, index) => (
+                <tr key={project.id} className="my-4 divide-y space-y-5">
+                  <td className="px-4 py-2">{index + 1}</td>
+                  <td className="px-4 py-2">{project.title}</td>
+                  <td className="px-4 py-2">{project.status}</td>
+                  <td className="px-4 py-2">{project.startDate}</td>
+                  <td className="px-4 py-2">{project.endDate}</td>
+                  <td className="px-4 py-2">{project.description}</td>
+                  <td className="px-4 py-2 underline text-blue-900">
+                    <Link to={`/projectexplore/${project.id}`}> Explore</Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </section>
-    </>
+    </section>
   );
 };
 
