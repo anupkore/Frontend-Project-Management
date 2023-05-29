3758f9b595bdfa1cfac526ae6b7cd43bd318fcaa
unknown <anup.kore88@gmail.com>
2023-05-29 14:20:58 +0530

    Form change

--- src/Components/AddNewProject.js
+++ src/Components/AddNewProject.js
@@ -1,120 +1,105 @@
-/*
-  This example requires some changes to your config:
+
+
+
+import {  Col, Container, Row } from "react-bootstrap";
+import Button from '@mui/material/Button';
+
+export default function AddNewProject() 
+{ 
+
+
   
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
 
-import { Col, Container, Row } from "react-bootstrap";
-import Button from "@mui/material/Button";
-
-export default function AddNewProject() {
   return (
     <>
-      <Container>
+       <Container>
         <Row>
-          {/* <Col>
+        {/* <Col>
         <div style={{width:"20rem",height:"14rem" }}>
           <img src="/Images/header.png" />
         </div>
         </Col> */}
-          <Col lg>
-            <div>
-              <div className="sm:mx-auto sm:w-full sm:max-w-sm my-4">
-                <h2 className="mt-10 text-center my-auto text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                  Create New Project
-                </h2>
+        <Col lg>
+        <div>
+            
+            <form action="#" method="POST" className="m-auto mt-16 max-w-l sm:mt-20">
+              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                
+                <div>
+                  <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                    Project Name
+                  </label>
+
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
               </div>
-              <form
-                action="#"
-                method="POST"
-                className="m-auto mt-16 max-w-l sm:mt-20"
-              >
-                <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                  <div>
-                    <label
-                      htmlFor="first-name"
-                      className="block text-sm font-semibold leading-6 text-gray-900"
-                    >
-                      Project Name
-                    </label>
-
-                    <div className="mt-2.5">
-                      <input
-                        type="text"
-                        name="first-name"
-                        id="first-name"
-                        autoComplete="given-name"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
-                      />
-                    </div>
+                
+                <div>
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Lead Name
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
                   </div>
-
-                  <div>
-                    <label
-                      htmlFor="last-name"
-                      className="block text-sm font-semibold leading-6 text-gray-900"
-                    >
-                      Lead Name
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="text"
-                        name="last-name"
-                        id="last-name"
-                        autoComplete="family-name"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
-                      />
-                    </div>
-                  </div>
+                </div>
                 </div>
 
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                  <div>
-                    <label
-                      htmlFor="first-name"
-                      className="block text-sm font-semibold leading-6 text-gray-900"
-                    >
-                      Risks
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="text"
-                        name="first-name"
-                        id="first-name"
-                        autoComplete="given-name"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
-                      />
-                    </div>
+                <div>
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Risks
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      autoComplete="given-name"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
                   </div>
-                  <div>
-                    <label
-                      htmlFor="last-name"
-                      className="block text-sm font-semibold leading-6 text-gray-900"
-                    >
-                      Mitigations
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="text"
-                        name="last-name"
-                        id="last-name"
-                        autoComplete="family-name"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
-                      />
-                    </div>
+                </div>
+                <div>
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Mitigations
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      autoComplete="family-name"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
                   </div>
+                </div>
+                
 
-                  <div className="grid grid-cols-1 gap-x-50 gap-y-6 sm:grid-cols-2">
-                  <div className="sm:col-span-2">
+                
+
+                <div className="sm:col-span-2">
                   <label
                     htmlFor="company"
                     className="block text-sm font-semibold leading-6 text-gray-900"
@@ -127,13 +112,11 @@ export default function AddNewProject() {
                       name="company"
                       id="company"
                       autoComplete="organization"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
-                  </div>
-                  </div>
-                  <div className="grid grid-cols-1 gap-x-50 gap-y-6 sm:grid-cols-2">
-                  <div className="sm:col-span-2">
+                </div>
+                <div className="sm:col-span-2">
                   <label
                     htmlFor="company"
                     className="block text-sm font-semibold leading-6 text-gray-900"
@@ -146,40 +129,38 @@ export default function AddNewProject() {
                       name="company"
                       id="company"
                       autoComplete="organization"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
-                  </div>
-                  </div>
+                </div>
 
-                  <div className="sm:col-span-2">
-                    <label
-                      htmlFor="company"
-                      className="block text-sm font-semibold leading-6 text-gray-900"
-                    >
-                      Description
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        type="text"
-                        name="company"
-                        id="company"
-                        autoComplete="organization"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
-                      />
-                    </div>
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="company"
+                    className="block text-sm font-semibold leading-6 text-gray-900"
+                  >
+                    Description
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      type="text"
+                      name="company"
+                      id="company"
+                      autoComplete="organization"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
                   </div>
                 </div>
-                <div className="flex mt-4 justify-end ">
-                  <Button variant="contained" className="">
-                    Create
-                  </Button>
-                </div>
-              </form>
-            </div>
-          </Col>
+
+              </div>
+              <Button variant="contained" className="justify-content-center" >
+        Create 
+      </Button>
+            </form>
+        </div>
+        </Col>
         </Row>
-      </Container>
+        </Container>
     </>
   );
 }
