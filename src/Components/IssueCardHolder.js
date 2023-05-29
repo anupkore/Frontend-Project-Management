68ef52e9ee11e17cbc223bb58632da2b9ceb9734
Varun Garade <varungarade1151@gmail.com>
2023-05-29 08:50:52 +0530

    Issue Dashboard

--- src/Components/IssueCardHolder.js
+++ src/Components/IssueCardHolder.js
@@ -19,20 +19,24 @@ function IssueCardHolder(props) {
 
   return (
     <>
-      <div id="content" className="scroll-pl-6 snap-x  mx-auto space-x-10">
-        <div className="w-64 rounded-4 bg-lime-100 h-max space-y-3 px-2 pb-4">
-          
-            <h1 className="text-center my-2 font-bold">{props.iss}</h1>
-
-            {issueCards.slice(0, 4).map((proj) => (
-              <IssueCard pro={proj}></IssueCard>
-            ))}
-            <div className="flex justify-end">
-              <Link to={`/issue/${props.iss}`}><h1 className="font-semibold">See More</h1></Link>
+      <div id="content" className="scroll-pl-6 snap-x mx-auto space-x-10">
+        <div className="w-64 rounded-4 bg-lime-100 h-max space-y-3 px-2 pb-2">
+          <div className="flex space-x-10 pt-3">
+            <div class="flex mx-auto space-x-2">
+              <span className="">{props.iss}</span>
+              <span className="badge bg-dark">{issueCards.length}</span>
             </div>
-            </div>
-           
           </div>
+          {issueCards.slice(0, 4).map((proj) => (
+            <IssueCard pro={proj}></IssueCard>
+          ))}
+          <div className="flex justify-end">
+            <Link to={`/issue/${props.iss}`}>
+              <h1 className="font-semibold">See More</h1>
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
