8f26aef0170960e9f5e646a4350ab2527c1f51c7
unknown <anup.kore88@gmail.com>
2023-05-30 12:24:38 +0530

    predefined workflows

--- src/Components/Workflow_1.js
+++ src/Components/Workflow_1.js
@@ -1,9 +1,28 @@
 function Workflow_1()
 {
+    const WorkflowImages=[{
+        W_ID:1,
+        W_Name:"/Images/workflow-1.png",
+        W_Path:`/Images/workflow-1.png`,
+    } , {
+        W_ID:2,
+        W_Name:"workflow-2.png",
+        W_Path:`/Images/workflow-2.png`,
+    } , {
+        W_ID:3,
+        W_Name:"workflow-3.png",
+        W_Path:`/Images/workflow-3.png`,
+    } , {
+        W_ID:4,
+        W_Name:"workflow-4.png",
+        W_Path:`/Images/workflow-4.png`,
+    }];
+    
+    
     return(
         <>
-            <div className="d-flex align-items-center justify-content-center">
-                <img src="/Images/workflow-1.png" ></img>
+            <div className="d-flex align-items-center justify-content-center" >
+                <img src={WorkflowImages[1].W_Path} ></img>
             </div>
         </>
     );
