92adc256f85f6db45ccd12ff026842b03333bc01
Yogesh Baiskar <yogeshjbaiskar100@gmail.com>
2023-06-09 14:05:05 +0530

    update form

--- src/Configurations/ParentConfiguration.js
+++ src/Configurations/ParentConfiguration.js
@@ -2,7 +2,8 @@ export default class ParentConfiguration
 {
     static parentURL=()=>
     {
-        const url = "https://poc-backend-rohanbhavsar215-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/";
+        // const url = "https://poc-backend-rohanbhavsar215-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/";
+        const url = "https://pmt-merge-1-simranpatil936-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/";
         return url;
     }
 }
