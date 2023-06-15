39643bc5219c60c8ea4996efb056bf85aaa58b36
Yashwant <yashrathore5598@gmail.com>
2023-06-15 10:02:21 +0530

    add new project form

--- src/Components/AddNewProject.js
+++ src/Components/AddNewProject.js
@@ -6,8 +6,7 @@ import { useState } from "react";
 
 import AuthenticationService from "../Services/AuthenticationService";
 
-export default function AddNewProject() 
-{
+export default function AddNewProject() {
   const [projectName, setProjectName] = useState("");
   const [description, setDescription] = useState("");
   const [clientName, setClientName] = useState("");
@@ -17,167 +16,136 @@ export default function AddNewProject()
   const [risks, setRisks] = useState("");
   const [mitigations, setMitigations] = useState("");
 
-  const[errorClientName , setErrorClientName] = useState("");
-  const[errorLeadName , setErrorLeadName] = useState('');
-  const[errorPlannedStartDate , setErrorPlannedStartDate] = useState('');
-  const[errorPlannedEndDate , setErrorPlannedEndDate] = useState('');
-  const[errorMessageProjectName , setErrorMessageProjectName] = useState('');
-  const[errorMessageDescription , setErrorMessageDescription] = useState('');
-  const[errorMessageRisks , setErrorMessagRisks] = useState('');
-  const[errorMessageMitigations , setErrorMessageMitigations] = useState('');
+  const [errorClientName, setErrorClientName] = useState("");
+  const [errorLeadName, setErrorLeadName] = useState("");
+  const [errorPlannedStartDate, setErrorPlannedStartDate] = useState("");
+  const [errorPlannedEndDate, setErrorPlannedEndDate] = useState("");
+  const [errorMessageProjectName, setErrorMessageProjectName] = useState("");
+  const [errorMessageDescription, setErrorMessageDescription] = useState("");
+  const [errorMessageRisks, setErrorMessagRisks] = useState("");
+  const [errorMessageMitigations, setErrorMessageMitigations] = useState("");
 
-    
+  function handleInputChangeProjectName(event) {
+    setProjectName(event.target.value);
+    setErrorMessageProjectName("");
+  }
 
-  function handleInputChangeProjectName(event) 
-     {
-        setProjectName(event.target.value);
-        setErrorMessageProjectName('');
+  function handleInputChangeDescription(event) {
+    setDescription(event.target.value);
+    setErrorMessageDescription("");
+  }
 
-     }
+  function handleInputChangeClientName(event) {
+    setClientName(event.target.value);
+    setErrorClientName("");
+  }
 
-     function handleInputChangeDescription(event) 
-     {
-        setDescription(event.target.value);
-        setErrorMessageDescription('');
-     }
+  function handleInputChangeLeadName(event) {
+    setLeadName(event.target.value);
+    setErrorLeadName("");
+  }
 
-     function handleInputChangeClientName(event) 
-     {
-        setClientName(event.target.value);
-        setErrorClientName('');
-     }
+  function handleInputChangePlannedStartDate(event) {
+    setPlannedStartDate(event.target.value);
+    setErrorPlannedStartDate("");
+  }
 
-     function handleInputChangeLeadName(event) 
-     {
-        setLeadName(event.target.value);
-        setErrorLeadName('');
-     }
+  function handleInputChangePlannedEndDate(event) {
+    setPlannedEndDate(event.target.value);
+    setErrorPlannedEndDate("");
+  }
 
-     function handleInputChangePlannedStartDate(event) 
-     {
-        setPlannedStartDate(event.target.value);
-        setErrorPlannedStartDate('');
-     }
+  function handleInputChangeRisks(event) {
+    setRisks(event.target.value);
+    setErrorMessagRisks("");
+  }
 
-     function handleInputChangePlannedEndDate(event) 
-     {
-        setPlannedEndDate(event.target.value);
-        setErrorPlannedEndDate('');
-     }
+  function handleInputChangeMitigations(event) {
+    setMitigations(event.target.value);
+    setErrorMessageMitigations("");
+  }
 
-     function handleInputChangeRisks(event) 
-     {
-        setRisks(event.target.value);
-        setErrorMessagRisks('');
-     }
+  function validateClientName(name) {
+    const nameRegex = /^[a-zA-Z\s'-]{2,50}$/;
+    return nameRegex.test(name);
+  }
 
-     function handleInputChangeMitigations(event) 
-     {
-        setMitigations(event.target.value);
-        setErrorMessageMitigations('');
-     }
+  // Function to validate the planned start date
+  function validatePlannedStartDate(startDate) {
+    const today = new Date();
+    const selectedDate = new Date(startDate);
 
-     function validateClientName(name) 
-     {
-      const nameRegex = /^[a-zA-Z\s'-]{2,50}$/;
-      return nameRegex.test(name);
-     }
+    // Check if the selected date is before today's date
+    return selectedDate >= today;
+  }
 
-     // Function to validate the planned start date
-      function validatePlannedStartDate(startDate) 
-      {
-        const today = new Date();
-        const selectedDate = new Date(startDate);
+  // Function to validate the planned end date
+  function validatePlannedEndDate(startDate, endDate) {
+    const startDateObj = new Date(startDate);
+    const endDateObj = new Date(endDate);
 
-        // Check if the selected date is before today's date
-        return selectedDate >= today;
-      }
+    // Check if the end date is before the start date
+    return endDateObj >= startDateObj;
+  }
 
-      // Function to validate the planned end date
-      function validatePlannedEndDate(startDate, endDate) 
-      {
-        const startDateObj = new Date(startDate);
-        const endDateObj = new Date(endDate);
-
-        // Check if the end date is before the start date
-        return endDateObj >= startDateObj;
-      }
-
-
-
-
-  function handleAddProject(event)
-  {
+  function handleAddProject(event) {
     event.preventDefault();
 
-    if (projectName.trim() === '') 
-    {
+    if (projectName.trim() === "") {
       setErrorMessageProjectName("This field is required");
       return;
     }
 
-    if (description.trim() === '') 
-    {
+    if (description.trim() === "") {
       setErrorMessageDescription("This field is required");
       return;
     }
-    
-    if (!validateClientName(clientName)) 
-    {
-      setErrorClientName('Please Enter Valid Client Name');
+
+    if (!validateClientName(clientName)) {
+      setErrorClientName("Please Enter Valid Client Name");
       return;
     }
-    if (!validateClientName(leadName)) 
-    {
-      setErrorLeadName('Please Enter Valid Lead Name');
+    if (!validateClientName(leadName)) {
+      setErrorLeadName("Please Enter Valid Lead Name");
       return;
     }
-    if (!validatePlannedStartDate(plannedStartDate)) 
-    {
-      setErrorPlannedStartDate('Please Enter valid Start Date');
-      return;
-    }
-  
-    if (!validatePlannedEndDate(plannedStartDate, plannedEndDate)) 
-    {
-      setErrorPlannedEndDate('Please Enter valid End Date');
+    if (!validatePlannedStartDate(plannedStartDate)) {
+      setErrorPlannedStartDate("Please Enter valid Start Date");
       return;
     }
 
-    if (risks.trim() === '') 
-    {
+    if (!validatePlannedEndDate(plannedStartDate, plannedEndDate)) {
+      setErrorPlannedEndDate("Please Enter valid End Date");
+      return;
+    }
+
+    if (risks.trim() === "") {
       setErrorMessagRisks("This field is required");
       return;
     }
 
-    if (mitigations.trim() === '') 
-    {
+    if (mitigations.trim() === "") {
       setErrorMessageMitigations("This field is required");
       return;
     }
-  
 
-    var payload = 
-    {
-        user_id: localStorage.getItem("UserID"),
-        project_name: projectName,
-        project_description: description,
-        planned_sd: plannedStartDate,
-        planned_ed: plannedEndDate,
-        client_name: clientName,
-        project_lead: leadName,
-        risk: risks,
-        mitigation: mitigations,
-        
-    }
+    var payload = {
+      user_id: localStorage.getItem("UserID"),
+      project_name: projectName,
+      project_description: description,
+      planned_sd: plannedStartDate,
+      planned_ed: plannedEndDate,
+      client_name: clientName,
+      project_lead: leadName,
+      risk: risks,
+      mitigation: mitigations,
+    };
     console.log(payload);
-    AuthenticationService.createProject(payload).then(()=>{
+    AuthenticationService.createProject(payload).then(() => {
       console.log("Hi Create Project");
-      window.location.href = '/allProjects';
-    })
+      window.location.href = "/allProjects";
+    });
   }
-  
-  
 
   return (
     <>
@@ -221,19 +189,14 @@ export default function AddNewProject()
                             autoComplete="given-name"
                             className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6 mb-2.5"
                           />
-                          <span className="text-danger">{errorMessageProjectName}</span>
+                          <span className="text-danger">
+                            {errorMessageProjectName}
+                          </span>
                         </div>
                       </div>
-                      
-                     
-
-                     
                     </div>
-                    
 
                     <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                     
-
                       <div className="grid grid-cols-1 gap-x-50 gap-y-6 sm:grid-cols-2">
                         <div className="sm:col-span-2">
                           <label
@@ -252,7 +215,9 @@ export default function AddNewProject()
                               autoComplete="organization"
                               className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
                             />
-                            <span className="text-danger">{errorPlannedStartDate}</span>
+                            <span className="text-danger">
+                              {errorPlannedStartDate}
+                            </span>
                           </div>
                         </div>
                       </div>
@@ -274,14 +239,13 @@ export default function AddNewProject()
                               autoComplete="organization"
                               className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
                             />
-                            <span className="text-danger">{errorPlannedEndDate}</span>
+                            <span className="text-danger">
+                              {errorPlannedEndDate}
+                            </span>
                           </div>
                         </div>
                       </div>
 
-
-                       
-                      
                       <div>
                         <label
                           htmlFor="first-name"
@@ -299,7 +263,9 @@ export default function AddNewProject()
                             autoComplete="given-name"
                             className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
                           />
-                          <span className="text-danger">{errorMessageRisks}</span>
+                          <span className="text-danger">
+                            {errorMessageRisks}
+                          </span>
                         </div>
                       </div>
 
@@ -320,7 +286,9 @@ export default function AddNewProject()
                             autoComplete="given-name"
                             className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-400 sm:text-sm sm:leading-6"
                           />
-                          <span className="text-danger">{errorMessageMitigations}</span>
+                          <span className="text-danger">
+                            {errorMessageMitigations}
+                          </span>
                         </div>
                       </div>
                       <div>
@@ -363,32 +331,38 @@ export default function AddNewProject()
                           <span className="text-danger">{errorClientName}</span>
                         </div>
                       </div>
-                       </div>
-                   
-                      <div className="col-span-full">
-              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                Description
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="about"
-                  name="about"
-                  value={description}
-                 onChange={handleInputChangeDescription}
-                  rows={3}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={''}
-                />
-              </div>
+                    </div>
 
-            </div>
+                    <div className="col-span-full">
+                      <label
+                        htmlFor="about"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Description
+                      </label>
+                      <div className="mt-2">
+                        <textarea
+                          id="about"
+                          name="about"
+                          value={description}
+                          onChange={handleInputChangeDescription}
+                          rows={3}
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          defaultValue={""}
+                        />
+                        <span className="text-danger">{errorMessageDescription}</span>
+                      </div>
+                    </div>
                     <div className="flex mt-5 justify-center ">
-                      <Button onClick={handleAddProject} variant="contained" className="">
+                      <Button
+                        onClick={handleAddProject}
+                        variant="contained"
+                        className=""
+                      >
                         Create
                       </Button>
                     </div>
                   </form>
-                  
                 </div>
               </Col>
             </Row>
